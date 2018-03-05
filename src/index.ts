c15f032d7e1ebea45bcd2cd2c512f4340ea4e7cd
Shu Uesugi <shu@chibicode.com>
2018-03-05 01:53:38 -0800

    Add random xkcd panel

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import {
-  JupyterLab, JupyterLabPlugin
-} from '@jupyterlab/application';
+import { JupyterLab, JupyterLabPlugin } from '@jupyterlab/application'
+import { ICommandPalette } from '@jupyterlab/apputils'
+import { Widget } from '@phosphor/widgets'
 
-import '../style/index.css';
-
+import '../style/index.css'
 
 /**
  * Initialization data for the jupyterlab_xkcd extension.
@@ -11,9 +10,33 @@ import '../style/index.css';
 const extension: JupyterLabPlugin<void> = {
   id: 'jupyterlab_xkcd',
   autoStart: true,
-  activate: (app: JupyterLab) => {
-    console.log('JupyterLab extension jupyterlab_xkcd is activated!');
-  }
-};
+  requires: [ICommandPalette],
+  activate: (app: JupyterLab, palette: ICommandPalette) => {
+    console.log('JupyterLab extension jupyterlab_xkcd is activated!')
 
-export default extension;
+    let widget: Widget = new Widget()
+    widget.id = 'xkcd-jupyterlab'
+    widget.title.label = 'xkcd.com'
+    widget.title.closable = true
+
+    const command: string = 'xkcd:open'
+    app.commands.addCommand(command, {
+      label: 'Random xkcd comic',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.addToMainArea(widget)
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id)
+      }
+    })
+
+    // Add the command to the palette.
+    palette.addItem({ command, category: 'Tutorial' })
+
+    console.log('ICommandPalette:', palette)
+  }
+}
+
+export default extension
